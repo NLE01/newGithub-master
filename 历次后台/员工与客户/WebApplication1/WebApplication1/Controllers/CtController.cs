9e235474f022e8f0860842e60b5af2aa5f4dd73b
NLE01 <1423463109@QQ.com>
2019-01-18 23:01:48 +0800

    历次后台

---
+++ 历次后台/员工与客户/WebApplication1/WebApplication1/Controllers/CtController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CtController : Controller
+    {
+        // GET: Ct
+        public ActionResult GitView()
+        {
+            Customer cto = new Customer();
+            cto.CustomerName = "客户01";
+            cto.Address = "???";
+            ViewBag.Customer = cto;
+            return View("CtoMyView", cto);
+        }
+    }
+}
---
+++ 历次后台/员工与客户/WebApplication1/WebApplication1/Controllers/TController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TController : Controller
+    {
+        // GET: T
+        public ActionResult Testes()
+        {
+            Employee emp = new Employee();
+            Customer cto = new Customer();
+            
+            cto.CustomerName = "客户01";
+            cto.Address = "???";
+            ViewBag.Customer = cto;
+
+            emp.Name = "程序员01";
+            emp.Salary = 20000;
+            ViewBag.Employee = emp;
+
+            ViewData["Employee"] = emp;
+            ViewData["Customer"] = cto;
+            return View("TView");
+        }
+    }
+}
